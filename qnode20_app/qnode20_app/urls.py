751253ba349c7ec41e19c5017c256eaa3f09f696
smartquailDev <smartquail.dev@gmail.com>
2022-05-22 00:40:03 -0500

    inicials

--- qnode20_app/qnode20_app/urls.py
+++ qnode20_app/qnode20_app/urls.py
@@ -1,21 +1,28 @@
-"""qnode20_app URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+#from django.contrib import admin
+from django.urls import path,re_path,include
+from django.conf import settings
+from baton.autodiscover import admin
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('baton/', include('baton.urls')),
+    #E-commerce-configs
+    #path('coupons/', include('coupons.urls', namespace='coupons')),
+    #path('cart/', include('cart.urls', namespace='cart')),
+    #path('orders/', include('orders.urls', namespace='orders')),
+    #path('payment/', include('payment.urls', namespace='payment')),
+    #path('shop/', include('shop.urls', namespace='shop')),
+    
+     
+    re_path(r'^smartbusinessmedia/', include(wagtailadmin_urls),name='wagtail'),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'', include(wagtail_urls)),
+
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
